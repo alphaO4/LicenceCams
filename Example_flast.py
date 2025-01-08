52a2e3f00d13a1c96e285cafd3bb8ec7b1c40202
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2025-01-08 01:55:13 +0100

    included a proxy, to avaid mixed content warning

--- Example_flast.py
+++ Example_flast.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template_string, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, Response
 import json
 import random
+import requests
 app = Flask(__name__)
 
 # Read JSON data
@@ -20,6 +21,14 @@ def api_streams():
 def api_random():
     random_cams = random.sample(sum_streams, 2)
     return random_cams
+@app.route("/proxy")
+def proxy():
+    url = request.args.get('url')
+    if not url:
+        return "No URL specified", 400
+    response = requests.get(url, stream=True)
+    return Response(response.iter_content(chunk_size=1024), content_type=response.headers.get('Content-Type'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
