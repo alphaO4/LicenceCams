bd15f7ad7d2c492c4025f5becdc8838829dede55
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2025-01-08 01:50:33 +0100

    First commit

--- Example_flast.py
+++ Example_flast.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template_string, jsonify, render_template
 import json
-
+import random
 app = Flask(__name__)
 
 # Read JSON data
@@ -9,11 +9,17 @@ with open("sum-streams.json", "r") as file:
 
 @app.route("/")
 def index():
-    return render_template("index.html", cameras=sum_streams)
+    random_cams = random.sample(sum_streams, 2)
+    return render_template("index.html", cameras=random_cams)
 
 @app.route("/api/streams")
 def api_streams():
     return jsonify(sum_streams)
+
+@app.route("/api/random")
+def api_random():
+    random_cams = random.sample(sum_streams, 2)
+    return random_cams
 
 if __name__ == "__main__":
     app.run(debug=True)
